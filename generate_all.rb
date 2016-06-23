c2ccf9b42d8161829d154d744c9fe7f4dbee9459
Struan Donald <struan@exo.org.uk>
2016-06-23 14:48:37 +0100

    Generate stats by looping over persons
    Build up a list of memberships per person and then use that when looping
    over each person to generate the list of stats per term.
    
    Included a monkey patched method on everypolitician-ruby to allow
    clearing the popolo data as otherwise the memory useage balloons.

--- generate_all.rb
+++ generate_all.rb
@@ -3,6 +3,17 @@ require 'json'
 require 'everypolitician'
 require 'everypolitician/popolo'
 
+
+# this is a bit of a kludge to make sure that we don't
+# use all the memory
+module EveryPolitician
+  class Legislature
+    def clear_popolo
+      @popolo = nil
+    end
+  end
+end
+
 gender_stats = []
 EveryPolitician.countries.each do |c|
   country = {
@@ -12,27 +23,36 @@ EveryPolitician.countries.each do |c|
   c.legislatures.each do |l|
     legislature = {
       slug: l.slug,
-      term: l.raw_data[:term],
     }
-    stats = {
-      overall: Hash.new(0),
-      parties: {},
-    }
-    legislature_data = l.popolo
-    legislature_data.organizations.where(classification: "party").each do |o|
-      stats[:parties][o.id] = Hash.new(0)
+    terms = {}
+
+    l.popolo.events.where(classification: "legislative period").each do |lp|
+      terms[lp.id] = {
+        overall: Hash.new(0),
+        parties: Hash.new { | hash, key | hash[key] = Hash.new(0) },
+      }
     end
-    legislature_data.memberships.each do |m|
-      person = legislature_data.persons.find_by(id: m.person_id)
-      gender = person.gender || 'unknown'
-      stats[:overall][gender] += 1
-      stats[:overall][:total] += 1
-      party = m.on_behalf_of_id
-      stats[:parties][party][gender] += 1
-      stats[:parties][party][:total] += 1
+
+    person_memberships = Hash.new { | hash, key | hash[key] = [] }
+    l.popolo.memberships.each do |m|
+      person_memberships[m.person_id].push(m)
     end
-    legislature[:stats] = stats
+
+    l.popolo.persons.each do |p|
+      gender = p.gender || 'unknown'
+      person_memberships[p.id].each do |m|
+        term_id = m.document[:legislative_period_id]
+        terms[term_id][:overall][gender] += 1
+        terms[term_id][:overall][:total] += 1
+        party = m.on_behalf_of_id
+        terms[term_id][:parties][party][gender] += 1
+        terms[term_id][:parties][party][:total] += 1
+      end
+    end
+
+    legislature[:terms] = terms
     legislatures.push(legislature)
+    l.clear_popolo
   end
   country[:legislatures] = legislatures
   gender_stats.push(country)
