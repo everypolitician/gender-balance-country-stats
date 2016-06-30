12f8c4cacfdfdf7f1111f874fc0da49da8887dc2
Chris Mytton <chrismytton@gmail.com>
2016-06-30 15:53:44 +0200

    Extract GenderStats class for easier testing
    Rather than have this as part of the background job make it a separate
    class so it can be tested from the repl.

--- app.rb
+++ app.rb
@@ -16,6 +16,80 @@ module EveryPolitician
   end
 end
 
+class GenderStats
+  def self.report
+    gender_stats = []
+    EveryPolitician.countries.take(1).each do |c|
+      country = {
+        slug: c.slug,
+      }
+      legislatures = []
+      c.legislatures.each do |l|
+        totals_seen = {}
+        totals = {
+          overall: Hash.new(0),
+          parties: Hash.new { | hash, key | hash[key] = Hash.new(0) },
+        }
+        legislature = {
+          slug: l.slug,
+        }
+        terms = {}
+
+        l.popolo.events.where(classification: "legislative period").each do |lp|
+          terms[lp.id] = {
+            overall: Hash.new(0),
+            parties: Hash.new { | hash, key | hash[key] = Hash.new(0) },
+          }
+        end
+
+        person_memberships = Hash.new { | hash, key | hash[key] = [] }
+        l.popolo.memberships.each do |m|
+          person_memberships[m.person_id].push(m)
+        end
+
+        l.popolo.persons.each do |p|
+          gender = p.gender || 'unknown'
+          if not totals_seen[p.id]
+              totals[:overall][gender] += 1
+              totals[:overall][:total] += 1
+              totals_seen[p.id] = gender
+          elsif totals_seen[p.id] != gender and gender != 'unknown'
+              totals[:overall][gender] += 1
+              totals[:overall][totals_seen[p.id]] -= 1
+              totals_seen[p.id] = gender
+          end
+          person_memberships[p.id].each do |m|
+            term_id = m.document[:legislative_period_id]
+            terms[term_id][:overall][gender] += 1
+            terms[term_id][:overall][:total] += 1
+            party = m.on_behalf_of_id
+            terms[term_id][:parties][party][gender] += 1
+            terms[term_id][:parties][party][:total] += 1
+            party_person = party + ':' + p.id
+            if not totals_seen[party_person]
+                totals[:parties][party][gender] += 1
+                totals[:parties][party][:total] += 1
+                totals_seen[party_person] = gender
+            elsif totals_seen[party_person] != gender and gender != 'unknown'
+                totals[:overall][party][gender] += 1
+                totals[:overall][party][totals_seen[party_person]] -= 1
+                totals_seen[party_person] = gender
+            end
+          end
+        end
+
+        legislature[:terms] = terms
+        legislature[:totals] = totals
+        legislatures.push(legislature)
+        l.clear_popolo
+      end
+      country[:legislatures] = legislatures
+      gender_stats.push(country)
+    end
+    gender_stats
+  end
+end
+
 class GBCountryStatsGenerator
   include Sidekiq::Worker
   include Everypoliticianbot::Github
@@ -23,77 +97,7 @@ class GBCountryStatsGenerator
   def perform(countries_json_url)
     Everypolitician.countries_json = countries_json_url
     with_git_repo('everypolitician/gender-balance-country-stats', branch: 'gh-pages', message: 'Update stats.json') do
-
-      gender_stats = []
-      EveryPolitician.countries.each do |c|
-        country = {
-          slug: c.slug,
-        }
-        legislatures = []
-        c.legislatures.each do |l|
-          totals_seen = {}
-          totals = {
-            overall: Hash.new(0),
-            parties: Hash.new { | hash, key | hash[key] = Hash.new(0) },
-          }
-          legislature = {
-            slug: l.slug,
-          }
-          terms = {}
-
-          l.popolo.events.where(classification: "legislative period").each do |lp|
-            terms[lp.id] = {
-              overall: Hash.new(0),
-              parties: Hash.new { | hash, key | hash[key] = Hash.new(0) },
-            }
-          end
-
-          person_memberships = Hash.new { | hash, key | hash[key] = [] }
-          l.popolo.memberships.each do |m|
-            person_memberships[m.person_id].push(m)
-          end
-
-          l.popolo.persons.each do |p|
-            gender = p.gender || 'unknown'
-            if not totals_seen[p.id]
-                totals[:overall][gender] += 1
-                totals[:overall][:total] += 1
-                totals_seen[p.id] = gender
-            elsif totals_seen[p.id] != gender and gender != 'unknown'
-                totals[:overall][gender] += 1
-                totals[:overall][totals_seen[p.id]] -= 1
-                totals_seen[p.id] = gender
-            end
-            person_memberships[p.id].each do |m|
-              term_id = m.document[:legislative_period_id]
-              terms[term_id][:overall][gender] += 1
-              terms[term_id][:overall][:total] += 1
-              party = m.on_behalf_of_id
-              terms[term_id][:parties][party][gender] += 1
-              terms[term_id][:parties][party][:total] += 1
-              party_person = party + ':' + p.id
-              if not totals_seen[party_person]
-                  totals[:parties][party][gender] += 1
-                  totals[:parties][party][:total] += 1
-                  totals_seen[party_person] = gender
-              elsif totals_seen[party_person] != gender and gender != 'unknown'
-                  totals[:overall][party][gender] += 1
-                  totals[:overall][party][totals_seen[party_person]] -= 1
-                  totals_seen[party_person] = gender
-              end
-            end
-          end
-
-          legislature[:terms] = terms
-          legislature[:totals] = totals
-          legislatures.push(legislature)
-          l.clear_popolo
-        end
-        country[:legislatures] = legislatures
-        gender_stats.push(country)
-      end
-
-      File.write('stats.json', gender_stats.to_json)
+      File.write('stats.json', GenderStats.report.to_json)
     end
   end
 end
